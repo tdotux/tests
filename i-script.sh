80a7b6a68b40992dab06f93bd79371168336ffa1
tdotux <105792562+tdotux@users.noreply.github.com>
2022-05-24 16:44:02 -0300

    Update i-script.sh

--- i-script.sh
+++ i-script.sh
@@ -4,27 +4,35 @@ timedatectl set-ntp true
 
 pacman -S e2fsprogs dosfstools nano wget --noconfirm
 
+
+
+PASTA_EFI=/sys/firmware/efi
+if [ ! -d "$PASTA_EFI" ];then
+echo -e "Sistema Legacy"
+parted /dev/sda mklabel mbr -s
+parted /dev/sda mkpart primary ext4 1MiB 100% -s
+parted /dev/sda set 1 boot on
+mkfs.ext4 /dev/sda2
+mount /dev/sda1 /mnt
+
+
+else
+echo -e "Sistema EFI"
 parted /dev/sda mklabel gpt -s
-
 parted /dev/sda mkpart primary fat32 1MiB 301MiB -s
-
 parted /dev/sda set 1 esp on
-
-parted /dev/sda mkpart primary btrfs 301MiB 100% -s
-
+parted /dev/sda mkpart primary ext4 301MiB 100% -s
 mkfs.fat -F32 /dev/sda1
-
-mkfs.btrfs -f /dev/sda2
-
+mkfs.ext4 /dev/sda2
 mount /dev/sda2 /mnt
-
 mkdir /mnt/boot/
-
 mkdir /mnt/boot/efi
-
 mount /dev/sda1 /mnt/boot/efi
+fi
 
-pacstrap /mnt base btrfs-progs linux-zen linux-firmware
+
+
+pacstrap /mnt base e2fsprogs linux-zen linux-firmware
 
 genfstab -U /mnt > /mnt/etc/fstab
 
