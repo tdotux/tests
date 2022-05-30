7279639a110a165d5a96b865670e4bffabe3070a
tdotux <105792562+tdotux@users.noreply.github.com>
2022-05-30 17:29:03 -0300

    Update pi-script.sh

--- pi-script.sh
+++ pi-script.sh
@@ -313,4 +313,4 @@ fallocate -l 4G /swapfile && chmod 600 /swapfile && mkswap /swapfile && swapon /
 esac
 
 
-#echo -e "$(tput bel)$(tput bold)$(tput setaf 7)$(tput setab 4)\n####INSTALAÇÃO CONCLUÍDA!!!\n"
+echo -e "$(tput bel)$(tput bold)$(tput setaf 7)$(tput setab 4)\n####INSTALAÇÃO CONCLUÍDA!!!\n"
