5111e1a5e5f5e4fb9ae3916aadd39038ebb7634e
tdotux <105792562+tdotux@users.noreply.github.com>
2022-05-31 14:49:34 -0300

    Delete test2.sh

--- test2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-read -p "Digite o Hostname : " HOST
-echo
-echo "$HOST" | sudo tee /etc/hostname
-
-echo -e "127.0.0.1 localhost.localdomain localhost\n::1 localhost.localdomain localhost\n127.0.1.1 $HOST.localdomain $HOST" | sudo tee /etc/hosts
