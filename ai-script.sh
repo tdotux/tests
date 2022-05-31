a4e51dec9af64f1936900cda57e16d5fa84b4dd0
tdotux <105792562+tdotux@users.noreply.github.com>
2022-05-31 16:03:42 -0300

    Update ai-script.sh

--- ai-script.sh
+++ ai-script.sh
@@ -23,7 +23,7 @@ parted /dev/sda mklabel gpt -s
 
 echo -e "$(tput bel)$(tput bold)$(tput setaf 7)$(tput setab 4)\n#### SISTEMA DE ARQUIVOS ####"
 
-echo -e "\n1 ==>> EXT4\n2 ==>> BTRFS\n"
+echo -e "\n\n1 - ext4\n2 - BTRFS\n\n"
 
 echo -ne "Escolha um Sistema de Arquivos : "
 
