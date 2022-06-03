75196609ccd6d0816834c99138e66b51ba274220
tdotux <105792562+tdotux@users.noreply.github.com>
2022-06-03 17:20:44 -0300

    Update epi-script.sh

--- epi-script.sh
+++ epi-script.sh
@@ -127,20 +127,6 @@ cp /etc/sudoers /etc/sudoers.bak && sed -i '82c\ %wheel ALL=(ALL:ALL) ALL' /etc/
 
 
 
-###GRUB
-
-PASTA_EFI=/sys/firmware/efi
-if [ ! -d "$PASTA_EFI" ];then
-echo -e "Sistema Legacy"
-grub-install --target=i386-pc /dev/sda --force && grub-mkconfig -o /boot/grub/grub.cfg
-
-else
-echo -e "Sistema EFI"
-pacman -S efibootmgr --noconfirm
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=Arch --removable && grub-mkconfig -o /boot/grub/grub.cfg
-
-fi
-
 
 
 ###DRIVER DE VIDEO
@@ -640,6 +626,24 @@ echo -e "$(tput sgr0)\n\n"
 ###USER DIRS UPDATE
 
 xdg-user-dirs-update
+
+
+
+
+
+###GRUB
+
+PASTA_EFI=/sys/firmware/efi
+if [ ! -d "$PASTA_EFI" ];then
+echo -e "Sistema Legacy"
+grub-install --target=i386-pc /dev/sda --force && grub-mkconfig -o /boot/grub/grub.cfg
+
+else
+echo -e "Sistema EFI"
+pacman -S efibootmgr --noconfirm
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=Arch --removable && grub-mkconfig -o /boot/grub/grub.cfg
+
+fi
 
 
 
