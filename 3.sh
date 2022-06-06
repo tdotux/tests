848e923cf54bfc04f07845ca01c3c37796e76a22
tdotux <105792562+tdotux@users.noreply.github.com>
2022-06-06 14:32:34 -0300

    Update 3.sh

--- 3.sh
+++ 3.sh
@@ -13,3 +13,16 @@ read -p "Digite o Nome de Usuário : " USERNAME
 arch-chroot /mnt useradd -m $USERNAME
 
 echo -e "$(tput sgr0)"
+
+
+###SENHA DO USUARIO
+
+echo -e "$(tput bel)$(tput bold)$(tput setaf 7)$(tput setab 4)"
+
+echo -e "Senha do Usuário"
+
+echo -e "\n"
+
+read -p "Digite a Senha de Usuário : " USERPASSWORD
+
+echo -e "$USERPASSWORD\n$USERPASSWORD" | arch-chroot passwd $USERNAME
