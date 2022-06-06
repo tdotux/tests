e0d213d6f73d70222909fa82c46857c40599ee07
tdotux <105792562+tdotux@users.noreply.github.com>
2022-06-06 13:51:16 -0300

    Update 3.sh

--- 3.sh
+++ 3.sh
@@ -4,57 +4,56 @@
 
 ###USERNAME
 
-arch-chroot /mnt <<EOL
 
-echo -e "$(tput bel)$(tput bold)$(tput setaf 7)$(tput setab 4)"
+arch-chroot /mnt bash -c ' echo -e "$(tput bel)$(tput bold)$(tput setaf 7)$(tput setab 4)"
 
-echo -e "Nome de Usuário (Username)"
+arch-chroot /mnt bash -c ' echo -e "Nome de Usuário (Username)"
 
-echo -e "\n"
+arch-chroot /mnt bash -c ' echo -e "\n"
 
-echo -ne "Digite o Nome do Usuário : "
+arch-chroot /mnt bash -c ' echo -ne "Digite o Nome do Usuário : "
 
-read USERNAME
+arch-chroot /mnt bash -c ' read USERNAME
 
-arch-chroot /mnt useradd -m $USERNAME
+arch-chroot /mnt bash -c ' arch-chroot /mnt useradd -m $USERNAME
 
-echo -e "$(tput sgr0)"
+arch-chroot /mnt bash -c ' echo -e "$(tput sgr0)"
 
 
 
 ###USER PASSWORD
 
-echo -e "$(tput bel)$(tput bold)$(tput setaf 7)$(tput setab 4)"
+#echo -e "$(tput bel)$(tput bold)$(tput setaf 7)$(tput setab 4)"
 
-echo -e "Senha do Usuário"
+#echo -e "Senha do Usuário"
 
-echo -e "\n"
+#echo -e "\n"
 
-echo -ne "Digite a Senha do Usuário : "
+#echo -ne "Digite a Senha do Usuário : "
 
-read USERPASSWORD
+#read USERPASSWORD
 
-arch-chroot /mnt echo -e '$USERPASSWORD\n$USERPASSWORD' | passwd $USERNAME
+#arch-chroot /mnt echo -e '$USERPASSWORD\n$USERPASSWORD' | passwd $USERNAME
 
-echo -e "$(tput sgr0)"
+#echo -e "$(tput sgr0)"
 
-EOL
+
 
 ###ROOT PASSWORD
 
-echo -e "$(tput bel)$(tput bold)$(tput setaf 7)$(tput setab 4)"
+#echo -e "$(tput bel)$(tput bold)$(tput setaf 7)$(tput setab 4)"
 
-echo -e "Senha de Root (Administrador)"
+#echo -e "Senha de Root (Administrador)"
 
-echo -e "\n"
+#echo -e "\n"
 
-echo -ne "Digite a Senha de Root : "
+#echo -ne "Digite a Senha de Root : "
 
-read ROOTPASSWORD
+#read ROOTPASSWORD
 
-arch-chroot /mnt echo -e '$ROOTPASSWORD\n$ROOTPASSWORD' | passwd
+#arch-chroot /mnt echo -e '$ROOTPASSWORD\n$ROOTPASSWORD' | passwd
 
-echo -e "$(tput sgr0)"
+#echo -e "$(tput sgr0)"
 
 
 
