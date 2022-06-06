d13f32623d4293a1ff058ddfc8f8982a2bb39cf0
tdotux <105792562+tdotux@users.noreply.github.com>
2022-06-06 14:24:57 -0300

    Update 3.sh

--- 3.sh
+++ 3.sh
@@ -15,45 +15,6 @@ echo -ne 'Digite o Nome do Usuário : "
 
 read USERNAME
 
-arch-chroot /mnt bash "useradd -m $USERNAME"
+arch-chroot /mnt useradd -m $USERNAME
 
 echo -e '$(tput sgr0)'
-
-
-
-###USER PASSWORD
-
-#echo -e "$(tput bel)$(tput bold)$(tput setaf 7)$(tput setab 4)"
-
-#echo -e "Senha do Usuário"
-
-#echo -e "\n"
-
-#echo -ne "Digite a Senha do Usuário : "
-
-#read USERPASSWORD
-
-#arch-chroot /mnt echo -e '$USERPASSWORD\n$USERPASSWORD' | passwd $USERNAME
-
-#echo -e "$(tput sgr0)"
-
-
-
-###ROOT PASSWORD
-
-#echo -e "$(tput bel)$(tput bold)$(tput setaf 7)$(tput setab 4)"
-
-#echo -e "Senha de Root (Administrador)"
-
-#echo -e "\n"
-
-#echo -ne "Digite a Senha de Root : "
-
-#read ROOTPASSWORD
-
-#arch-chroot /mnt echo -e '$ROOTPASSWORD\n$ROOTPASSWORD' | passwd
-
-#echo -e "$(tput sgr0)"
-
-
-
