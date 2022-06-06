3a787b7e02d5a87dbf6cbaa01f0dd8fe99479c6a
tdotux <105792562+tdotux@users.noreply.github.com>
2022-06-06 17:11:53 -0300

    Update manual-script-2.sh

--- manual-script-2.sh
+++ manual-script-2.sh
@@ -981,12 +981,12 @@ arch-chroot /mnt xdg-user-dirs-update
 PASTA_EFI=/sys/firmware/efi
 if [ ! -d "$PASTA_EFI" ];then
 echo -e "Sistema Legacy"
-arch-chroot /mnt grub-install --target=i386-pc /dev/sda --force && grub-mkconfig -o /boot/grub/grub.cfg
+arch-chroot /mnt grub-install --target=i386-pc /dev/sda --force && arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 
 else
 echo -e "Sistema EFI"
 pacman -S efibootmgr --noconfirm
-arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=Arch --removable && grub-mkconfig -o /boot/grub/grub.cfg
+arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=Arch --removable && arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 
 fi
 
