f5fad894e371d38bf5bcda46871283fa36620e01
tdotux <105792562+tdotux@users.noreply.github.com>
2022-06-06 10:54:35 -0300

    Create 1.sh

---
+++ 1.sh
@@ -0,0 +1,197 @@
+#!/usr/bin/env sh
+
+###SINCRONIZAR RELOGIO COM A INTERNET
+
+timedatectl set-ntp true
+
+
+
+###UTILITARIOS BASICOS
+
+pacman -S dosfstools nano wget --noconfirm
+
+
+
+###DETECTAR UEFI OU LEGACY
+
+PASTA_EFI=/sys/firmware/efi
+
+if [ -d "$PASTA_EFI" ];then
+
+echo -e "Sistema EFI"
+
+parted /dev/sda mklabel gpt -s
+
+echo -e "$(tput bel)$(tput bold)$(tput setaf 7)$(tput setab 4)"
+
+echo -e "#### Sistema de Arquivos ####"
+
+echo -e "\n\n"
+
+echo -e "1 - Ext4"
+
+echo -e "\n"
+
+echo -e "2 - Btrfs"
+
+echo -e "\n"
+
+echo -e "3 - F2FS"
+
+echo -e "\n"
+
+echo -e "4 - XFS"
+
+echo -e "\n\n"
+
+echo -ne "Escolha um Sistema de Arquivos : "
+
+read -n1 -s ARQUIVOS
+
+case $ARQUIVOS in
+
+"1")
+echo "Ext4"
+sleep 2
+echo -e "$(tput sgr0)"
+parted /dev/sda mkpart primary fat32 1MiB 301MiB -s
+parted /dev/sda set 1 esp on
+parted /dev/sda mkpart primary ext4 301MiB 100% -s
+mkfs.fat -F32 /dev/sda1
+mkfs.ext4 -F /dev/sda2
+mount /dev/sda2 /mnt
+mkdir /mnt/boot/
+mkdir /mnt/boot/efi
+mount /dev/sda1 /mnt/boot/efi
+
+;;
+
+"2")
+echo "Btrfs"
+sleep 2
+echo -e "$(tput sgr0)"
+parted /dev/sda mkpart primary fat32 1MiB 301MiB -s
+parted /dev/sda set 1 esp on
+parted /dev/sda mkpart primary btrfs 301MiB 100% -s
+mkfs.fat -F32 /dev/sda1
+mkfs.btrfs -f /dev/sda2
+mount /dev/sda2 /mnt
+mkdir /mnt/boot/
+mkdir /mnt/boot/efi
+mount /dev/sda1 /mnt/boot/efi
+
+;;
+
+"3")
+echo "F2FS"
+sleep 2
+echo -e "$(tput sgr0)"
+parted /dev/sda mkpart primary fat32 1MiB 301MiB -s
+parted /dev/sda set 1 esp on
+parted /dev/sda mkpart primary f2fs 301MiB 100% -s
+mkfs.fat -F32 /dev/sda1
+mkfs.f2fs -f /dev/sda2
+mount /dev/sda2 /mnt
+mkdir /mnt/boot/
+mkdir /mnt/boot/efi
+mount /dev/sda1 /mnt/boot/efi
+
+;;
+
+"4")
+echo "XFS"
+sleep 2
+echo -e "$(tput sgr0)"
+parted /dev/sda mkpart primary fat32 1MiB 301MiB -s
+parted /dev/sda set 1 esp on
+parted /dev/sda mkpart primary btrfs 301MiB 100% -s
+mkfs.fat -F32 /dev/sda1
+mkfs.xfs -f /dev/sda2
+mount /dev/sda2 /mnt
+mkdir /mnt/boot/
+mkdir /mnt/boot/efi
+mount /dev/sda1 /mnt/boot/efi
+esac
+
+
+else
+
+echo -e "Sistema Legacy"
+
+parted /dev/sda mklabel msdos -s
+
+echo -e "$(tput bel)$(tput bold)$(tput setaf 7)$(tput setab 4)"
+
+echo -e "#### Sistema de Arquivos ####"
+
+echo -e "\n\n"
+
+echo -e "1 - Ext4"
+
+echo -e "\n"
+
+echo -e "2 - Btrfs"
+
+echo -e "\n"
+
+echo -e "3 - F2FS"
+
+echo -e "\n"
+
+echo -e "4 - XFS"
+
+echo -e "\n\n"
+
+echo -ne "Escolha um Sistema de Arquivos : "
+
+read -n1 -s ARQUIVOS
+
+case $ARQUIVOS in
+
+"1")
+echo "Ext4"
+sleep 2
+echo -e "$(tput sgr0)"
+parted /dev/sda mkpart primary ext4 1MiB 100% -s
+parted /dev/sda set 1 boot on
+mkfs.ext4 -F /dev/sda1
+mount /dev/sda1 /mnt
+
+;;
+
+"2")
+echo "Btrfs"
+sleep 2
+echo -e "$(tput sgr0)"
+parted /dev/sda mkpart primary btrfs 1MiB 100% -s
+parted /dev/sda set 1 boot on
+mkfs.btrfs -f /dev/sda1
+mount /dev/sda1 /mnt
+
+;;
+
+"3")
+echo "F2FS"
+sleep 2
+echo -e "$(tput sgr0)"
+parted /dev/sda mkpart primary f2fs 1MiB 100% -s
+parted /dev/sda set 1 boot on
+mkfs.f2fs -f /dev/sda1
+mount /dev/sda1 /mnt
+
+;;
+
+"4")
+echo "XFS"
+sleep 2
+echo -e "$(tput sgr0)"
+parted /dev/sda mkpart primary xfs 1MiB 100% -s
+parted /dev/sda set 1 boot on
+mkfs.xfs -f /dev/sda1
+mount /dev/sda1 /mnt
+
+esac
+
+fi
+
+echo -e "$(tput sgr0)"
