fa7a9ebc3c2081fead24d9900f35690ce603fa13
tdotux <105792562+tdotux@users.noreply.github.com>
2022-06-10 11:41:33 -0300

    Add files via upload

---
+++ vol-2.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+
+###UTILITARIOS BASICOS
+
+pacman -S dosfstools nano wget --noconfirm
+
+
+
+###DETECTAR UEFI OU LEGACY
+
+PASTA_EFI=/sys/firmware/efi
+
+if [ -d "$PASTA_EFI" ];then
+
+echo -e "Sistema EFI"
+
+parted /dev/sda mklabel gpt -s
+
+echo -e "$(tput bel)$(tput bold)$(tput setaf 7)$(tput setab 4)"
+
+
+
+
+printf '\x1bc';
+PS3=$'\nSelecione um Sistema de Arquivos: ';
+echo -e '### Sistema de Arquivos ### '
+select filesystem in {EXT4,BTRFS,F2FS,XFS};do
+	case $filesystem in
+	EXT4|BTRFS|F2FS|XFS)
+	parted /dev/sda mkpart primary fat32 1MiB 301MiB -s
+    parted /dev/sda set 1 esp on
+    parted /dev/sda mkpart primary ${filesystem,,} 301MiB 100% -s
+    *) echo -e "\e[1;38mErro\e[m\nEscolha uma Opção válida.";continue;;
+	esac
+break;
+done
+
+mkfs.fat -F32 /dev/sda1
+
+##
+
+fs=(sudo blkid -o value -s TYPE /dev/sda2)
+
+if "$fs" = "ext4"
+mkfs.ext4 -F /dev/sda2
+
+elif
+mkfs.$fs -f /dev/sda2
+fi
+
+mount /dev/sda2 /mnt
+mkdir /mnt/boot/
+mkdir /mnt/boot/efi
+mount /dev/sda1 /mnt/boot/efi
+
+
+
+### Driver de Video
+
+printf '\x1bc';
+PS3=$'\nSelecione um Driver de Vídeo: ';
+echo -e '### Driver de Vídeo ### '
+select drive in {AMDGPU,ATI,INTEL,Nouveau,Nvidia,VMWARE};do
+	case $drive in
+	AMDGPU|ATI|INTEL|Nouveau|Nvidia|VMWARE)
+	sudo pacman -S xf86-video-${drive,,} ;;
+	*) echo -e "\e[1;38mErro\e[m\nEscolha uma Opção válida.";continue;;
+	esac
+break;
+done
+
+
+### KERNEL
+
+printf '\x1bc';
+PS3=$'\nSelecione um Kernel: ';
+echo -e '### Kernel ### '
+select kernel in {STABLE,ZEN,LTS,HARDENED};do
+	case $kernel in
+	STABLE|ZEN|LTS|HARDENED)
+	pacstrap /mnt base btrfs-progs dosfstools e2fsprogs f2fs-tools dosfstools xfsprogs linux-firmware ${kernel,,} ;;
+	*) echo -e "\e[1;38mErro\e[m\nEscolha uma Opção válida.";continue;;
+	esac
+break;
+done
