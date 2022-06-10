24796d94a94c4c6bea7b04856b515289d1679832
tdotux <105792562+tdotux@users.noreply.github.com>
2022-06-10 13:56:13 -0300

    Update vol-2.sh

--- vol-2.sh
+++ vol-2.sh
@@ -5,7 +5,7 @@ echo -e 'Escolha um Driver de Vídeo: '
 select drive in {AMDGPU,ATI,INTEL,Nouveau,Nvidia,VMWARE};do
 	case $drive in
 	AMDGPU|ATI|INTEL|Nouveau|Nvidia|VMWARE)
-	echo -e "Opção: ${drive,,}\nexecute:\npacman -S xf86-video-${drive,,}";;
+	pacman -S xf86-video-${drive,,}";;
 	*) echo -e "\e[1;38mErro\e[m\nEscolha uma Opção válida.";continue;;
 	esac
 break;
