174dfc0f0777229ffa2d180c9c0cc4dd0962806f
tdotux <105792562+tdotux@users.noreply.github.com>
2022-06-10 14:32:58 -0300

    Update vol-2.sh

--- vol-2.sh
+++ vol-2.sh
@@ -23,16 +23,18 @@ select filesystem in {ext4,btrfs,F2fs,xfs};do
 	parted /dev/sda set 1 esp on
 	parted /dev/sda mkpart primary ${filesystem,,} 301MiB 100% -s
 	mkfs.${filesystem,,} -f /dev/sda2;;
-	mount /dev/sda2 /mnt
-	mkdir /mnt/boot/
-	mkdir /mnt/boot/efi
-	mount /dev/sda1 /mnt/boot/efi
 	
 	*) echo -e "\e[1;38mErro\e[m\nEscolha uma Opção válida.";continue;;
 	esac
 break;
 done 
 
+
+
+mount /dev/sda2 /mnt
+mkdir /mnt/boot/
+mkdir /mnt/boot/efi
+mount /dev/sda1 /mnt/boot/efi
 
 
 
