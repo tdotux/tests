6e7fb622e55df40784b91b8cc810cdf06e441017
tdotux <105792562+tdotux@users.noreply.github.com>
2022-06-10 14:30:51 -0300

    Update vol-2.sh

--- vol-2.sh
+++ vol-2.sh
@@ -17,11 +17,17 @@ echo -e 'Escolha um Sistema de Arquivos: '
 select filesystem in {ext4,btrfs,F2fs,xfs};do
 	case $filesystem in
 	btrfs|f2fs|xfs)
+	
 	parted /dev/sda mklabel gpt -s
 	parted /dev/sda mkpart primary fat32 1MiB 301MiB -s
 	parted /dev/sda set 1 esp on
 	parted /dev/sda mkpart primary ${filesystem,,} 301MiB 100% -s
 	mkfs.${filesystem,,} -f /dev/sda2;;
+	mount /dev/sda2 /mnt
+	mkdir /mnt/boot/
+	mkdir /mnt/boot/efi
+	mount /dev/sda1 /mnt/boot/efi
+	
 	*) echo -e "\e[1;38mErro\e[m\nEscolha uma Opção válida.";continue;;
 	esac
 break;
