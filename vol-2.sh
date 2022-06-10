8ffb97d5335f644c2c99df83820455bd56e7201f
tdotux <105792562+tdotux@users.noreply.github.com>
2022-06-10 14:13:35 -0300

    Update vol-2.sh

--- vol-2.sh
+++ vol-2.sh
@@ -4,6 +4,37 @@
 
 
 
+printf '\x1bc';
+PS3=$'\nSelecione uma opção: ';
+echo -e 'Escolha um Sistema de Arquivos: '
+select filesystem in {EXT4|BTRFS|F2FS|XFS};do
+	case $filesystem in
+	BTRFS|F2FS|XFS)
+	mkfs.${filesystem,,} -f /dev/sda2;;
+	*) echo -e "\e[1;38mErro\e[m\nEscolha uma Opção válida.";continue;;
+	esac
+break;
+done 
+
+
+
+
+printf '\x1bc';
+PS3=$'\nSelecione uma opção: ';
+echo -e 'Escolha um Driver de Vídeo: '
+select drive in {AMDGPU,ATI,INTEL,Nouveau,Nvidia,VMWARE};do
+	case $drive in
+	AMDGPU|ATI|INTEL|Nouveau|Nvidia|VMWARE)
+	pacman -S xf86-video-${drive,,};;
+	*) echo -e "\e[1;38mErro\e[m\nEscolha uma Opção válida.";continue;;
+	esac
+break;
+done 
+
+
+
+
+
 
 printf '\x1bc';
 PS3=$'\nSelecione uma opção: ';
@@ -20,18 +51,3 @@ done
 
 
 
-
-
-
-
-printf '\x1bc';
-PS3=$'\nSelecione uma opção: ';
-echo -e 'Escolha um Driver de Vídeo: '
-select drive in {AMDGPU,ATI,INTEL,Nouveau,Nvidia,VMWARE};do
-	case $drive in
-	AMDGPU|ATI|INTEL|Nouveau|Nvidia|VMWARE)
-	pacman -S xf86-video-${drive,,};;
-	*) echo -e "\e[1;38mErro\e[m\nEscolha uma Opção válida.";continue;;
-	esac
-break;
-done 
