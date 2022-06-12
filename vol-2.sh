fef7304674156667bbec0807bdb55a13b436bbc6
tdotux <105792562+tdotux@users.noreply.github.com>
2022-06-12 13:54:15 -0300

    Update vol-2.sh

--- vol-2.sh
+++ vol-2.sh
@@ -41,6 +41,109 @@ mount /dev/sda1 /mnt/boot/efi
 
 
 
+### DRIVER DE VIDEO
+
+
+printf '\x1bc';
+PS3=$'\nSelecione uma opção: ';
+echo -e 'Escolha um Driver de Vídeo: '
+select drive in {AMDGPU,ATI,INTEL,Nouveau,Nvidia,VMWARE};do
+	case $drive in
+	AMDGPU|ATI|INTEL|Nouveau|Nvidia|VMWARE)
+	echo -e "${drive,,}\nOK";;
+	*) echo -e "\e[1;38mErro\e[m\nEscolha uma Opção válida.";continue;;
+	esac
+break;
+done
+
+
+
+###HOSTNAME
+
+echo -e "$(tput bel)$(tput bold)$(tput setaf 7)$(tput setab 4)"
+
+echo -e "Hostname (Nome do PC)"
+
+echo -e "\n"
+
+read -p "Digite o Hostname : " HOSTNAME
+
+echo -e "$(tput sgr0)"
+
+
+
+echo -e "\n\n\n"
+
+
+
+###USERNAME
+
+echo -e "$(tput bel)$(tput bold)$(tput setaf 7)$(tput setab 4)"
+
+echo -e "Nome de Usuário (Username)"
+
+echo -e "\n"
+
+read -p "Digite o Nome de Usuário : " USERNAME
+
+echo -e "$(tput sgr0)"
+
+
+
+echo -e "\n\n\n"
+
+
+
+###SENHA DO USUARIO
+
+echo -e "$(tput bel)$(tput bold)$(tput setaf 7)$(tput setab 4)"
+
+echo -e "Senha do Usuário"
+
+echo -e "\n"
+
+stty -echo
+
+printf "Digite a Senha de Usuário: "
+
+read USERPASSWORD
+
+stty echo
+
+echo -e "$(tput sgr0)"
+
+
+
+echo -e "\n\n\n"
+
+
+
+### SENHA DE ROOT
+
+echo -e "$(tput bel)$(tput bold)$(tput setaf 7)$(tput setab 4)"
+
+echo -e "Senha de Root (Administrador)"
+
+echo -e "\n"
+
+stty -echo
+
+printf "Digite a Senha de Root: "
+
+read ROOTPASSWORD
+
+
+
+stty echo
+
+echo -e "$(tput sgr0)"
+
+
+
+echo -e "\n\n\n"
+
+
+
 ### KERNEL
 
 
@@ -58,14 +161,321 @@ done
 
 
 
-printf '\x1bc';
-PS3=$'\nSelecione uma opção: ';
-echo -e 'Escolha um Driver de Vídeo: '
-select drive in {AMDGPU,ATI,INTEL,Nouveau,Nvidia,VMWARE};do
-	case $drive in
-	AMDGPU|ATI|INTEL|Nouveau|Nvidia|VMWARE)
-	echo -e "Opção: ${drive,,}\nOK";;
-	*) echo -e "\e[1;38mErro\e[m\nEscolha uma Opção válida.";continue;;
-	esac
-break;
-done 
+###FSTAB
+
+genfstab -U /mnt > /mnt/etc/fstab
+
+
+###ADD-USER
+
+
+arch-chroot /mnt useradd -m $USERNAME
+
+echo -e "$(tput sgr0)"
+
+
+###SET-USER-PASSWORD
+
+
+echo -e "$USERPASSWORD\n$USERPASSWORD" | arch-chroot /mnt passwd $USERNAME
+
+
+
+### SET-ROOT-PASSWORD
+
+echo -e "$ROOTPASSWORD\n$ROOTPASSWORD" | arch-chroot /mnt passwd
+
+
+
+### SET-HOSTNAME-AND-CONFIGURE-HOSTS
+
+echo -e "$HOSTNAME" > /mnt/etc/hostname
+
+echo -e "127.0.0.1 localhost.localdomain localhost\n::1 localhost.localdomain localhost\n127.0.1.1 $HOSTNAME.localdomain $HOSTNAME" | arch-chroot /mnt tee /etc/hosts
+
+
+
+
+##### INSIDE CHROOT #####
+
+
+
+###AJUSTAR HORA AUTOMATICAMENTE
+
+arch-chroot /mnt timedatectl set-ntp true
+
+
+
+###SINCRONIZAR REPOSITORIOS
+
+arch-chroot /mnt pacman -Syy git --noconfirm
+
+
+###UTILITARIOS BASICOS
+
+arch-chroot /mnt pacman -Sy nano wget pacman-contrib reflector sudo grub --noconfirm
+
+
+
+###MIRRORS
+
+#cp /mnt/etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist.bak && curl -s "https://archlinux.org/mirrorlist/?country=BR&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - | tee /mnt/etc/pacman.d/mirrorlist && sed -i '/br.mirror.archlinux-br.org/d' /mnt/etc/pacman.d/mirrorlist
+
+
+
+
+###PARALLEL DOWNLOADS
+
+cp /mnt/etc/pacman.conf /mnt/etc/pacman.conf.bak && sed -i '37c\ParallelDownloads = 16' /mnt/etc/pacman.conf && arch-chroot /mnt pacman -Syyyuuu --noconfirm
+
+
+
+
+###MULTILIB
+
+sed -i '93c\[multilib]' /mnt/etc/pacman.conf && sed -i '94c\Include = /etc/pacman.d/mirrorlist' /mnt/etc/pacman.conf && arch-chroot /mnt pacman -Syyyuu --noconfirm
+
+
+
+
+###FUSO HORARIO
+
+ln -sf /mnt/usr/share/zoneinfo/America/Sao_Paulo /mnt/etc/localtime && arch-chroot /mnt hwclock --systohc
+
+
+
+
+###LOCALE
+
+mv /mnt/etc/locale.gen /mnt/etc/locale.gen.bak && echo -e 'pt_BR.UTF-8 UTF-8' | tee /mnt/etc/locale.gen && arch-chroot /mnt locale-gen && echo -e 'LANG=pt_BR.UTF-8' | tee /mnt/etc/locale.conf
+
+
+
+
+###GRUPOS
+
+arch-chroot /mnt groupadd -r autologin
+
+arch-chroot /mnt groupadd -r sudo
+
+arch-chroot /mnt usermod -G autologin,sudo,wheel,lp $USERNAME
+
+
+
+
+###WHEEL
+
+cp /mnt/etc/sudoers /mnt/etc/sudoers.bak && sed -i '82c\ %wheel ALL=(ALL:ALL) ALL' /mnt/etc/sudoers
+
+
+
+
+###SET-PRIMARY-VIDEO-DRIVER
+
+arch-chroot /mnt pacman -S xf86-video-${drive,,}
+
+
+
+###SET-DESKTOP-ENVIRONMENT
+
+
+
+if [ "$DE" = "1" ];then
+
+echo "Budge"
+
+echo -e "$(tput sgr0)\n\n"
+
+##Pacotes Padrão
+
+arch-chroot /mnt pacman -S xorg-server xorg-xinit xterm networkmanager tar gzip bzip2 zip unzip unrar p7zip pipewire pipewire-alsa pipewire-jack pipewire-pulse wireplumber xdg-user-dirs gnome-disk-utility neofetch --noconfirm
+
+##Interface e DM
+
+arch-chroot /mnt pacman -S budgie-desktop gnome-terminal gedit gnome-calculator gnome-calendar gnome-system-monitor nautilus network-manager-applet lightdm lightdm-gtk-greeter --noconfirm
+arch-chroot /mnt systemctl enable lightdm NetworkManager
+
+
+
+elif [ "$DE" = "2" ];then
+
+echo "Cinnamon"
+
+echo -e "$(tput sgr0)\n\n"
+
+##Pacotes Padrão
+
+arch-chroot /mnt pacman -S xorg-server xorg-xinit xterm networkmanager tar gzip bzip2 zip unzip unrar p7zip pipewire pipewire-alsa pipewire-jack pipewire-pulse wireplumber xdg-user-dirs gnome-disk-utility neofetch --noconfirm
+
+##Interface e DM
+
+arch-chroot /mnt pacman -S cinnamon network-manager-applet lightdm lightdm-gtk-greeter --noconfirm
+arch-chroot /mnt systemctl enable lightdm NetworkManager
+
+
+
+elif [ "$DE" = "3" ];then
+
+echo "Deepin"
+
+echo -e "$(tput sgr0)\n\n"
+
+##Pacotes Padrão
+
+arch-chroot /mnt pacman -S xorg-server xorg-xinit xterm networkmanager tar gzip bzip2 zip unzip unrar p7zip pipewire pipewire-alsa pipewire-jack pipewire-pulse wireplumber xdg-user-dirs gnome-disk-utility neofetch --noconfirm
+
+##Interface e DM
+
+arch-chroot /mnt pacman -S deepin network-manager-applet lightdm lightdm-gtk-greeter --noconfirm
+arch-chroot /mnt systemctl enable lightdm NetworkManager
+
+
+
+elif [ "$DE" = "4" ];then
+
+echo "Gnome"
+
+echo -e "$(tput sgr0)\n\n"
+
+##Pacotes Padrão
+
+arch-chroot /mnt pacman -S xorg-server xorg-xinit xterm networkmanager tar gzip bzip2 zip unzip unrar p7zip pipewire pipewire-alsa pipewire-jack pipewire-pulse wireplumber xdg-user-dirs gnome-disk-utility neofetch --noconfirm
+
+##Interface e DM
+
+arch-chroot /mnt pacman -S gnome gnome-tweaks network-manager-applet gdm --noconfirm
+arch-chroot /mnt systemctl enable gdm NetworkManager
+
+elif [ "$DE" = "5" ];then
+
+echo "KDE Plasma (X11)"
+
+echo -e "$(tput sgr0)\n\n"
+
+##Pacotes Padrão
+
+arch-chroot /mnt pacman -S xorg-server xorg-xinit xterm networkmanager tar gzip bzip2 zip unzip unrar p7zip pipewire pipewire-alsa pipewire-jack pipewire-pulse wireplumber xdg-user-dirs gnome-disk-utility neofetch --noconfirm
+
+##Interface e DM
+
+arch-chroot /mnt pacman -S plasma konsole sddm dolphin spectacle kcalc kwrite gwenview plasma-nm plasma-pa --noconfirm
+arch-chroot /mnt systemctl enable sddm NetworkManager
+
+
+elif [ "$DE" = "6" ];then
+
+echo "KDE Plasma (Wayland)"
+
+echo -e "$(tput sgr0)\n\n"
+
+##Pacotes Padrão
+
+arch-chroot /mnt pacman -S xorg-server xorg-xinit xterm networkmanager tar gzip bzip2 zip unzip unrar p7zip pipewire pipewire-alsa pipewire-jack pipewire-pulse wireplumber xdg-user-dirs gnome-disk-utility neofetch --noconfirm
+
+##Interface e DM
+
+arch-chroot /mnt pacman -S plasma konsole sddm dolphin spectacle kcalc kwrite gwenview plasma-nm plasma-pa plasma-wayland-session --noconfirm
+arch-chroot /mnt systemctl enable sddm NetworkManager
+
+elif [ "$DE" = "7" ];then
+
+echo "LXDE"
+
+echo -e "$(tput sgr0)\n\n"
+
+##Pacotes Padrão
+
+arch-chroot /mnt pacman -S xorg-server xorg-xinit xterm networkmanager tar gzip bzip2 zip unzip unrar p7zip pipewire pipewire-alsa pipewire-jack pipewire-pulse wireplumber xdg-user-dirs gnome-disk-utility neofetch --noconfirm
+
+##Interface e DM
+
+arch-chroot /mnt pacman -S lxde-gtk3 lxtask network-manager-applet lightdm lightdm-gtk-greeter --noconfirm
+arch-chroot /mnt systemctl enable lightdm NetworkManager
+
+elif [ "$DE" = "8" ];then
+
+echo "LXQT"
+
+echo -e "$(tput sgr0)\n\n"
+
+##Pacotes Padrão
+
+arch-chroot /mnt pacman -S xorg-server xorg-xinit xterm networkmanager tar gzip bzip2 zip unzip unrar p7zip pipewire pipewire-alsa pipewire-jack pipewire-pulse wireplumber xdg-user-dirs gnome-disk-utility neofetch --noconfirm
+
+##Interface e DM
+
+arch-chroot /mnt pacman -S lxqt lxtask network-manager-applet sddm --noconfirm
+arch-chroot /mnt systemctl enable sddm NetworkManager
+
+elif [ "$DE" = "9" ];then
+
+echo "MATE"
+
+echo -e "$(tput sgr0)\n\n"
+
+##Pacotes Padrão
+
+arch-chroot /mnt pacman -S xorg-server xorg-xinit xterm networkmanager tar gzip bzip2 zip unzip unrar p7zip pipewire pipewire-alsa pipewire-jack pipewire-pulse wireplumber xdg-user-dirs gnome-disk-utility neofetch --noconfirm
+
+##Interface e DM
+
+arch-chroot /mnt pacman -S mate mate-extra network-manager-applet lightdm lightdm-gtk-greeter --noconfirm
+arch-chroot /mnt systemctl enable lightdm NetworkManager
+
+
+
+elif [ "$DE" = "10" ];then
+
+echo "XFCE"
+
+echo -e "$(tput sgr0)\n\n"
+
+##Pacotes Padrão
+
+arch-chroot /mnt pacman -S xorg-server xorg-xinit xterm networkmanager tar gzip bzip2 zip unzip unrar p7zip pipewire pipewire-alsa pipewire-jack pipewire-pulse wireplumber xdg-user-dirs gnome-disk-utility neofetch --noconfirm
+
+##Interface e DM
+
+arch-chroot /mnt pacman -S xfce4 xfce4-screenshooter xfce4-pulseaudio-plugin xfce4-whiskermenu-plugin xarchiver lxtask ristretto mousepad galculator thunar-archive-plugin network-manager-applet lightdm lightdm-gtk-greeter --noconfirm
+arch-chroot /mnt systemctl enable lightdm NetworkManager
+
+fi
+
+
+echo -e "$(tput sgr0)\n\n"
+
+
+
+
+
+###USER DIRS UPDATE
+
+arch-chroot /mnt xdg-user-dirs-update
+
+
+
+
+
+###GRUB
+
+PASTA_EFI=/sys/firmware/efi
+if [ ! -d "$PASTA_EFI" ];then
+echo -e "Sistema Legacy"
+arch-chroot /mnt grub-install --target=i386-pc /dev/sda --force && arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
+
+else
+echo -e "Sistema EFI"
+pacman -S efibootmgr --noconfirm
+arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=Arch --removable && arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
+
+fi
+
+
+
+echo -e "$(tput bel)$(tput bold)$(tput setaf 7)$(tput setab 4)"
+
+
+echo -e "Instalação Concluída!!!!!"
+
+
+echo -e "$(tput sgr0)"
