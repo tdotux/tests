aa3dd878d42c32fe772baa48522a10edd99ea928
tdotux <105792562+tdotux@users.noreply.github.com>
2022-06-12 14:51:58 -0300

    Update and rename vol-2.sh to vol-1.sh

---
+++ vol-1.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+
+
+
+### UTILITARIOS BASICOS
+
+pacman -S dosfstools nano wget --noconfirm
+
+
+
+
+### SISTEMA DE ARQUIVOS
+
+
+printf '\x1bc';
+PS3=$'\nSelecione uma opção: ';
+echo -e 'Escolha um Sistema de Arquivos: '
+select filesystem in {ext4,btrfs,F2fs,xfs};do
+	case $filesystem in
+	btrfs|f2fs|xfs)
+	
+	parted /dev/sda mklabel gpt -s
+	parted /dev/sda mkpart primary fat32 1MiB 301MiB -s
+	parted /dev/sda set 1 esp on
+	parted /dev/sda mkpart primary ${filesystem,,} 301MiB 100% -s
+	mkfs.${filesystem,,} -f /dev/sda2;;
+	
+	*) echo -e "\e[1;38mErro\e[m\nEscolha uma Opção válida.";continue;;
+	esac
+break;
+done 
+
+
+
+mount /dev/sda2 /mnt
+mkdir /mnt/boot/
+mkdir /mnt/boot/efi
+mount /dev/sda1 /mnt/boot/efi
+
+
+
+
+
