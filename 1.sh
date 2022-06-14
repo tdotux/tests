6bbc84018bc8f0ad2c2d0b93c37b5eed860ea586
tdotux <105792562+tdotux@users.noreply.github.com>
2022-06-14 10:21:51 -0300

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -8,6 +8,13 @@ pacman -S dosfstools nano wget --noconfirm
 
 
 
+parted /dev/sda mklabel gpt -s
+parted /dev/sda mkpart primary fat32 1MiB 301MiB -s
+parted /dev/sda set 1 esp on
+mkfs.fat -F32 /dev/sda1
+
+
+
 ### SISTEMA DE ARQUIVOS
 
 
@@ -17,14 +24,8 @@ echo -e 'Escolha um Sistema de Arquivos: '
 select filesystem in {ext4,btrfs,F2fs,xfs};do
 	case $filesystem in
 	btrfs|f2fs|xfs)
-	
-	parted /dev/sda mklabel gpt -s
-	parted /dev/sda mkpart primary fat32 1MiB 301MiB -s
-	parted /dev/sda set 1 esp on
 	parted /dev/sda mkpart primary ${filesystem,,} 301MiB 100% -s
-	mkfs.${filesystem,,} -f /dev/sda2;;
-	mkfs.fat -F32 /dev/sda1
-	
+	mkfs.${filesystem,,} -f /dev/sda2;;	
 	*) echo -e "\e[1;38mErro\e[m\nEscolha uma Opção válida.";continue;;
 	esac
 break;
