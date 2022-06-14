cb1af984de28ef72db8a52ab5e20b207d52a0502
tdotux <105792562+tdotux@users.noreply.github.com>
2022-06-14 10:16:24 -0300

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
+
 ### UTILITARIOS BASICOS
 
 pacman -S dosfstools nano wget --noconfirm
@@ -22,6 +23,7 @@ select filesystem in {ext4,btrfs,F2fs,xfs};do
 	parted /dev/sda set 1 esp on
 	parted /dev/sda mkpart primary ${filesystem,,} 301MiB 100% -s
 	mkfs.${filesystem,,} -f /dev/sda2;;
+	mkfs.fat -F32 /dev/sda1
 	
 	*) echo -e "\e[1;38mErro\e[m\nEscolha uma Opção válida.";continue;;
 	esac
