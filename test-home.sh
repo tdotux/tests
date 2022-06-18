52bd3ec1baabc504632641cf9d0d5722668dddb1
tdotux <105792562+tdotux@users.noreply.github.com>
2022-06-18 19:59:40 -0300

    Add files via upload

---
+++ test-home.sh
@@ -0,0 +1,41 @@
+#!/bin/env sh
+
+printf '\x1bc';
+PS3=$'\nSelecione uma opção: ';
+echo -e 'Escolha um Sistema de Arquivos: '
+select filesystem in {ext4,btrfs,F2fs,xfs};do
+	case $filesystem in
+
+	ext4)
+	echo -e "${filesystem,,}";;
+
+	btrfs|f2fs|xfs)
+	echo -e "${filesystem,,}";;
+
+	*) echo -e "\e[1;38mErro\e[m\nEscolha uma Opção válida.";continue;;
+	esac
+break;
+done
+
+
+printf '\x1bc';
+PS3=$'\nSelecione uma opção: ';
+echo -e 'Deseja criar uma partição /home separada? '
+
+select separatehome in {Sim,Não};do
+	case $separatehome in
+
+	Sim)
+    select homedevice in "$devices";
+    select fshome in "$filesystem"; do
+
+    parted ${homedevice,,} mkpart primary ${fshome,,} 1MiB 100% -s
+    done
+
+	Não)
+	echo -e "${separatehome,,}";;
+
+	*) echo -e "\e[1;38mErro\e[m\nEscolha uma Opção válida.";continue;;
+	esac
+break;
+done
