64880cfa56094b6b1762a9db06bfa87b093f0160
tdotux <105792562+tdotux@users.noreply.github.com>
2022-06-22 17:39:30 -0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+devices_list=($(lsblk -nd --output NAME,SIZE))
 
-devices_list=($(lsblk -d | grep 'sd\|hd\|vd\|nvme\|mmcblk'))
+devices_select=($(lsblk -d | grep 'sd\|hd\|vd\|nvme\|mmcblk'))
 
 printf '\x1bc';
 PS3=$'\nSelecione uma opção: ';
+echo -e "$devices_list"
 echo -e 'Escolha um Disco: '
-select device in $devices_list; do
+select device in $devices_select; do
 echo "$device";
 break
 
