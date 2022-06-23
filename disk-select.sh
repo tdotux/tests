59938135a39f757051aa536ca85e13efe1d5964e
tdotux <105792562+tdotux@users.noreply.github.com>
2022-06-23 10:24:00 -0300

    Update disk-select.sh

--- disk-select.sh
+++ disk-select.sh
@@ -1,10 +1,20 @@
-#!/bin/bash
+#!/bin/sh
 
-devices_list=($(lsblk -d | awk '{print "/dev/" $1}' | grep 'sd\|hd\|vd\|nvme\|mmcblk'))
+
+devices_list=$(lsblk -dlnp -I 2,3,8,9,22,34,56,57,58,65,66,67,68,69,70,71,72,91,128,129,130,131,132,133,134,135,259 | awk '{print $1,$4,$6,$7}' | column -t)
+
+devices_select=$(lsblk -nd --output NAME | grep 'sd\|hd\|vd\|nvme\|mmcblk')
 
 printf '\x1bc';
 PS3=$'\nSelecione uma opção: ';
-echo -e 'Escolha um Disco para Instalar o Sistema: '
-select device in $devices_list; do
-echo "$device"
+echo -e "Lista de Dispositivos:"
+echo -e "\n"
+echo -e "Nome - Tamanho - Tipo"
+echo -e "$devices_list"
+echo -e "\n"
+echo -e 'Escolha um Disco: '
+select device in $devices_select; do
+echo "$device";
+break
+
 done
