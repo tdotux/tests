39780e8925f819117a1518c0b277c5f02c5b0775
tdotux <105792562+tdotux@users.noreply.github.com>
2022-06-24 10:48:20 -0300

    Update cli-test.sh

--- cli-test.sh
+++ cli-test.sh
@@ -245,6 +245,22 @@ fi
 
 
 
+### SWAP
+
+printf '\x1bc';
+
+echo -e "SWAP"
+echo -e "Para Máquinas até 8GB de RAM - 4GB de SWAP"
+echo -e "\n"
+echo -e "Acima de 8GB de RAM - 2GB de SWAP"
+echo -e "Digite o NÚMERO correspondente a quantidade de SWAP em GB"
+echo -e "Exemplo: Para 4GB de SWAP - Digite 4"
+
+read -p "SWAP em GB : " SWAP
+
+
+
+
 ### DRIVER DE VIDEO
 
 printf '\x1bc';
@@ -655,6 +671,22 @@ printf '\x1bc';
 echo "Digite e Repita a Senha de ROOT"
 
 arch-chroot /mnt passwd
+
+
+
+
+###SET-SWAP
+
+if [ "$filesystem" = "btrfs" ];then
+
+arch-chroot /mnt truncate -s 0 /swapfile && arch-chroot /mnt chattr +C /swapfile && arch-chroot /mnt btrfs property set /swapfile compression "" && arch-chroot /mnt fallocate -l ${SWAP,,}G /swapfile && arch-chroot /mnt chmod 600 /swapfile && arch-chroot /mnt mkswap /swapfile && arch-chroot /mnt swapon /swapfile && echo -e '/swapfile none swap defaults 0 0\n' | arch-chroot /mnt tee -a /etc/fstab
+
+else
+
+arch-chroot /mnt fallocate -l ${SWAP,,}G /swapfile && arch-chroot /mnt chmod 600 /swapfile && arch-chroot /mnt mkswap /swapfile && arch-chroot /mnt swapon /swapfile && echo -e '/swapfile none swap defaults 0 0\n' | arch-chroot /mnt tee -a /etc/fstab
+
+fi
+
 
 
 echo -e "$(tput bel)$(tput bold)$(tput setaf 7)$(tput setab 4)"
