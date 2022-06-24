744d2c0f34c3703fc182f07568c042392f0c4211
tdotux <105792562+tdotux@users.noreply.github.com>
2022-06-24 10:22:10 -0300

    Update cli-test.sh

--- cli-test.sh
+++ cli-test.sh
@@ -279,6 +279,19 @@ break;
 done
 
 
+### SWAP
+
+printf '\x1bc';
+
+echo -e "SWAP"
+echo -e "Para Máquinas até 8GB de RAM - 4GB de SWAP"
+echo -e "\n"
+echo -e "Acima de 8GB de RAM - 2GB de SWAP"
+echo -e "Digite o NÚMERO correspondente a quantidade de SWAP em GB"
+echo -e "Exemplo: Para 4GB de SWAP - Digite 4"
+
+read -p "SWAP em GB : " SWAP
+
 
 
 
@@ -631,6 +644,19 @@ fi
 ###USER DIRS UPDATE
 
 arch-chroot /mnt xdg-user-dirs-update
+
+
+###SET-SWAP
+
+if [ "$filesystem" = "btrfs" ];then
+
+arch-chroot /mnt truncate -s 0 /swapfile && arch-chroot /mnt chattr +C /swapfile && btrfs property set /swapfile compression "" && arch-chroot /mnt fallocate -l ${SWAP,,}G /swapfile && arch-chroot /mnt chmod 600 /swapfile && arch-chroot /mnt mkswap /swapfile && arch-chroot /mnt swapon /swapfile && echo -e '/swapfile none swap defaults 0 0\n' | arch-chroot /mnt tee -a /etc/fstab
+
+else
+
+arch-chroot /mnt fallocate -l ${SWAP,,}G /swapfile && arch-chroot /mnt chmod 600 /swapfile && arch-chroot /mnt mkswap /swapfile && arch-chroot /mnt swapon /swapfile && echo -e '/swapfile none swap defaults 0 0\n' | arch-chroot /mnt tee -a /etc/fstab
+
+fi
 
 
 
